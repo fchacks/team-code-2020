327065dd61ac40d1d87e3e68a2a807f49959ca6f
Sanjay Seshan <sanjay.seshan@outlook.com>
2020-11-22 19:50:23 -0500

    Create rpi.py

---
+++ rpi-arduino-workshop/rpi.py
@@ -0,0 +1,10 @@
+# Blink LED code
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+while True:
+  GPIO.output(8, GPIO.HIGH) 
+  time.sleep(1) 
+  GPIO.output(8, GPIO.LOW)
+  time.sleep(1)
